da1673550097fe071508103b877f3c4b64ac32ba
MARK FERNANDO <ksfernando@deakin.edu.au>
2019-08-28 21:01:46 +1000

    v2

---
+++ BudgetMates/BudgetMates/BudgetMates/View/ViewBill.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BudgetMates.View
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ViewBill : ContentPage
+	{
+		public ViewBill ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
